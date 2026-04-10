8a8c3febad5412a2adc9b7a073ebed20cd31d5b6
fatpat81 <patshouse1981@gmail.com>
2026-04-10 16:33:43 -0400

    Delete convert.ps1

--- convert.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile("c:\Users\pat\Documents\Warhammer App\wh40k_logo.png")
-$bmp = new-object System.Drawing.Bitmap($img)
-$iconStream = new-object System.IO.FileStream("c:\Users\pat\Documents\Warhammer App\icon.ico", [System.IO.FileMode]::Create)
-$icon = [System.Drawing.Icon]::FromHandle($bmp.GetHicon())
-$icon.Save($iconStream)
-$iconStream.Close()
-$bmp.Dispose()
-$img.Dispose()
